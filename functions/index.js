6747c7197b244b1fc9815a68e8f13619c279d3fd
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-09-24 12:16:22 +0200

    Register method

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,65 @@
 const functions = require('firebase-functions')
+const app = require('express')()
 
 const admin = require('firebase-admin')
 
 admin.initializeApp()
 
-const express = require('express')
-const app = express()
+const firebaseConfig = {
+	apiKey: 'AIzaSyCkLt1KjtOdJYZTahNX7OQBnSc7s1HKOj8',
+	authDomain: 'odopass-test.firebaseapp.com',
+	databaseURL: 'https://odopass-test.firebaseio.com',
+	projectId: 'odopass-test',
+	storageBucket: 'odopass-test.appspot.com',
+	messagingSenderId: '890270410291',
+	appId: '1:890270410291:web:c8de4f68ea91c4636d6546',
+	measurementId: 'G-KHKSVY39YG',
+}
 
-app.post('/doc', (req, res) => {
-	const newDoc = {
-		body: req.body.body,
-		userHandle: req.body.userHandle,
+const firebase = require('firebase')
+firebase.initializeApp(firebaseConfig)
+
+//TEST POST DOC FIREBASE
+// app.post('/doc', (req, res) => {
+// 	const newDoc = {
+// 		body: req.body.body,
+// 		userHandle: req.body.userHandle,
+// 	}
+
+// 	admin
+// 		.firestore()
+// 		.collection('doc')
+// 		.add(newDoc)
+// 		.then((doc) => {
+// 			return res.json({ message: `document ${doc.id} created successfully` })
+// 		})
+// 		.catch((err) => {
+// 			res.status(500).json({ error: 'Something went wrong' })
+// 			console.error(err)
+// 		})
+// })
+
+//REGISTER API
+app.post('/register', (req, res) => {
+	const newUser = {
+		email: req.body.email,
+		password: req.body.password,
+		confirmPassword: req.body.password,
 	}
 
-	admin
-		.firestore()
-		.collection('doc')
-		.add(newDoc)
-		.then((doc) => {
-			return res.json({ message: `document ${doc.id} created successfully` })
+	firebase
+		.auth()
+		.createUserWithEmailAndPassword(newUser.email, newUser.password)
+		.then((data) => {
+			return res
+				.status(201)
+				.json({ message: `user ${data.user.uid} signed up successfully` }) //201 => ressource created
 		})
 		.catch((err) => {
-			res.status(500).json({ error: 'Something went wrong' })
 			console.error(err)
+			return res.status(400).json({ error: err.code }) //400 => server error
 		})
 })
 
-//https/baseurl.com/api/... convention
+//https/baseurl.com/api/... => convention    europe-west2- Londres
 exports.api = functions.region('europe-west2').https.onRequest(app)
