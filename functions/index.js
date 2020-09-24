7ed03cde49f4f2e8f47a67c0c4cef1bb6a44ae39
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-09-24 22:35:02 +0200

    Register user and adding to db collection

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,9 @@ const functions = require('firebase-functions')
 const app = require('express')()
 
 const admin = require('firebase-admin')
-
 admin.initializeApp()
+
+const db = admin.firestore() //database firestore
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyCkLt1KjtOdJYZTahNX7OQBnSc7s1HKOj8',
@@ -42,22 +43,62 @@ firebase.initializeApp(firebaseConfig)
 //REGISTER API
 app.post('/register', (req, res) => {
 	const newUser = {
+		username: req.body.username,
 		email: req.body.email,
 		password: req.body.password,
 		confirmPassword: req.body.password,
 	}
 
-	firebase
-		.auth()
-		.createUserWithEmailAndPassword(newUser.email, newUser.password)
-		.then((data) => {
-			return res
-				.status(201)
-				.json({ message: `user ${data.user.uid} signed up successfully` }) //201 => ressource created
+	let token //token validation for each user
+
+	db.doc(`/users/${newUser.username}`)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				return res.status(400).json({ message: 'username already exists' })
+			} else {
+				return firebase
+					.auth()
+					.createUserWithEmailAndPassword(newUser.email, newUser.password) //creation user
+			}
 		})
+
+		// .then((data) => {
+		// 	return res
+		// 		.status(201)
+		// 		.json({ message: `user ${data.user.uid} signed up successfully` }) //201 => resource created
+		// })
+
+		.then((data) => {
+			//token identification generated
+			userId = data.user.uid
+			return data.user.getIdToken()
+		})
+
+		.then((tokenId) => {
+			token = tokenId
+
+			const userCredentials = {
+				//user details information add to db
+				username: newUser.username,
+				email: newUser.email,
+				createdAt: new Date().toISOString(),
+				userId,
+			}
+
+			return db.doc(`/users/${userId}`).set(userCredentials) //adding user credentials to db
+		})
+		.then(() => {
+			return res.status(201).json({ token })
+		})
+
 		.catch((err) => {
 			console.error(err)
-			return res.status(400).json({ error: err.code }) //400 => server error
+			if (err.code === 'auth/email-already-in-use') {
+				return res.status(400).json({ message: 'This email is already use.' }) //400 => request error client
+			} else {
+				return res.status(500).json({ error: err.code }) //500 => internal server error (request)
+			}
 		})
 })
 
