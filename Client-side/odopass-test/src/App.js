8d142c700b75c8caaeabbf7c8fb5952157e21a00
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-09-25 15:12:29 +0200

    Setup basic app template

--- Client-side/odopass-test/src/App.js
+++ Client-side/odopass-test/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+//Style
+import './App.scss'
+
+//Pages
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import UserProfile from './Pages/UserProfile'
+import UsersList from './Pages/UsersList'
 
 function App() {
-	return <div className='App'>Hello there</div>
+	return (
+		<div className='App'>
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Login} />
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/profile-user' component={UserProfile} />
+					<Route exact path='/users-list' component={UsersList} />
+				</Switch>
+			</Router>
+		</div>
+	)
 }
 
 export default App
---
+++ Client-side/odopass-test/src/Pages/UserProfile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function UserProfiles() {
+	return <div></div>
+}
+
+export default UserProfiles
